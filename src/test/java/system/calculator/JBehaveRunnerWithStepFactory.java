b2af19ae416ee8772a937a9142acfb25dbc2c9a2
kir <remigiusz_dudek@tlen.pl>
2013-12-11 06:19:11 +0100

    removed codecentric

--- src/test/java/system/calculator/JBehaveRunnerWithStepFactory.java
+++ src/test/java/system/calculator/JBehaveRunnerWithStepFactory.java
@@ -1,18 +1,15 @@
 package system.calculator;
 
-import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
-import org.junit.runner.RunWith;
 import system.CommonJBheaveConfiguration;
 import system.calculator.scenario.FieldCalculatorWithRegularVariableScenario;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RunWith(JUnitReportingRunner.class)
 public class JBehaveRunnerWithStepFactory extends JUnitStories {
 
     @Override
